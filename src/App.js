6acdc4bce07d8b2dc09d56b6f8599744dcfe5d88
laurill <lahdesmakilauri@gmail.com>
2023-02-26 22:37:27 +0200

    Exchange rates

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import './App.css'
 
 const URL = 'https://api.exchangerate.host/latest'
 
-
 function App() {
 
   const [eur, setEur] = useState(0)
   const [gbp, setGbp] = useState(0)
   const [rate, setRate] = useState(0)
-
 
 async function convert(e) {
 
